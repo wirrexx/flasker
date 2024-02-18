ede417749092014506c972e53ccdcfd91a75dc83
wirrexx <wirrexx@gmail.com>
2024-02-18 10:22:26 +0100

    added new packages

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, request, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'thisismysecretkey'
+
+
+
+# Create a formclass, kalla på en klass när vi kallar på den. validators vill ha något skrivet
+class NamerForm(FlaskForm):
+	name =  StringField("What's your name? ", validators=[DataRequired()])
+	submit = SubmitField("Submit")
+
+
 
 
 @app.route('/')
@@ -28,6 +41,20 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template('500.html'), 500
+
+
+# Create name page
+@app.route('/name', methods=['GET', 'POST'])
+def create_name():
+	name = None
+	form = NamerForm()
+	# validate form
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('name.html', 
+		name=name,
+		form=form)
 
 
 if __name__=='__main__':
