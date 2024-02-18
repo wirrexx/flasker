d0033443b60317045b73ffe5fc4b9a9e20f8dc0d
wirrexx <wirrexx@gmail.com>
2024-02-18 09:20:37 +0100

    Added venv and requested

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ def user(name):
 def page_not_found(e):
 	return render_template('404.html'), 404
 
-# Internal server error
+# Internal server error message
 @app.errorhandler(500)
 def page_not_found(e):
 	return render_template('500.html'), 500
 
 
 if __name__=='__main__':
-	app.run(debug=True)
+	app.run(debug=True)	
